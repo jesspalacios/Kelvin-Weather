cc20efef0e32d5ebae478ca2d4a8c84fa280d8db
mzlovelyj <liljp064@gmail.com>
2017-10-06 11:34:09 -0400

    updated a note line6

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const kelvin = prompt('Hey, so what is the Kelvin temperature today?');
 //Asking the user to enter tempt in Kelvin
 
 const celsius = kelvin - 273;
-//conversation for Kelvin to Celsius by subtracting 273 from the kelvin variable. FYI 71 F is 295
+//conversation for Kelvin to Celsius by subtracting 273 from the kelvin variable. 
+// FYI 71 F is 295 K
 
 let fahrenheit = celsius * (9 / 5) + 32;
 //Calculating Fahrenheit converting celsisus to Fahrenheit & rouding
